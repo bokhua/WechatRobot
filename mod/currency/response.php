1042ea74cba154c1ea1cac196112329c7a7b3272
bokhua <bohua@live.com>
2016-07-15 17:41:28 +0800

    no message

--- mod/currency/response.php
+++ mod/currency/response.php
@@ -31,7 +31,7 @@ class mod_currency
 		foreach ($pairs as $pair) {
 			$pair = '"'.$pair.'"';
 		}
-		$keyword = urlencode(implode(',', $pair));
+		$keyword = urlencode(implode(',', $pairs));
 		
 		$url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20('.$keyword.')&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
 		
